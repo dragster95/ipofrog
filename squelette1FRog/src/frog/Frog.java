00060d0b1f028d4a5d0b8492214549d731ae98ad
dragster95 <mickaelmarianayagam@gmail.com>
2021-11-03 22:03:49 +0100

    bonjour

--- squelette1FRog/src/frog/Frog.java
+++ squelette1FRog/src/frog/Frog.java
@@ -2,10 +2,14 @@ package frog;
 
 import gameCommons.Game;
 import gameCommons.IFrog;
+import util.Direction;
 
 public class Frog implements IFrog {
 	
 	private Game game;
 
+	@Override
+	public void move(Direction key) {
 
+	}
 }
